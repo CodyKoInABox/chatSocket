7477f57fc8d6e81ebebcbd8b42f07c9b6e9508f9
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-30 15:14:34 -0300

    support for render deployment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('https://chatsocketbackend.onrender.com')
 
 const messageForm = document.getElementById('messageForm')
 const messageInput = document.getElementById('messageInput')
